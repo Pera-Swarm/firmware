6420794c7d609f736d8ce40ce57b86d58072ec03
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-02-19 08:12:53 +0530

    Virtual sensor disabling support

--- firmware/mqtt/mqtt_distance.cpp
+++ firmware/mqtt/mqtt_distance.cpp
@@ -10,52 +10,53 @@ uint16_t dist_virt;
 #ifdef ENABLE_MQTT
 
 void mqtt_distance_handle(char* msg){
-    //Serial.printf("distance update from server: %d -> %d \n", atoi(g[3]), atoi(msg));
-
-    dist_lock = 0;
-    dist_virt=atoi(msg);
+   #ifdef ENABLE_VIRT_READINGS
+   //Serial.printf("distance update from server: %d -> %d \n", atoi(g[3]), atoi(msg));
+   dist_lock = 0;
+   dist_virt=atoi(msg);
+   #endif
 }
 
 int distance_read(){
 
-    // Get physical sensor reading
-    uint16_t dist_phy = 0;
-    uint16_t dist_final;
+   uint16_t dist_final;
 
-    for(int i=0;i<3;i++){
-        // obtain 3 reedings and get the average
-        dist_phy += distance.getDistanceInt();
-        delay(50);
-    }
-    dist_phy = (dist_phy/3)/10; // in cm
+   // Get physical sensor reading
+   uint16_t dist_phy = 0;
+   for(int i=0;i<3;i++) dist_phy += distance.getDistanceInt();
+   dist_phy = (dist_phy/3)/10; // in cm
 
-    // Publish: sensor/distance
-    //      { "id":0, "dist":0 }
-    sprintf(tempString1, "%s/%s", CHANNEL,TOPIC_DISTANCE_REQ_TO_SERVER);
-    sprintf(tempString2, "{\"id\":\"%d\",\"dist\":\"%d\"}", mqtt_robot_id, dist_phy);
+   #ifdef ENABLE_VIRT_READINGS
+   // Publish: sensor/distance
+   //      { "id":0, "dist":0 }
+   sprintf(tempString1, "%s/%s", CHANNEL,TOPIC_DISTANCE_REQ_TO_SERVER);
+   sprintf(tempString2, "{\"id\":\"%d\",\"dist\":\"%d\"}", mqtt_robot_id, dist_phy);
 
-    dist_lock=1;
-    dist_virt=-1;
-    mqtt_publish(tempString1, tempString2, false);
+   dist_lock=1;
+   dist_virt=-1;
 
-    mqtt_wait(&dist_lock);
+   mqtt_publish(tempString1, tempString2, false);
+   mqtt_wait(&dist_lock);
 
-    if(dist_lock == 0){
-        // have both sensor readings
-        dist_final =  (dist_phy < dist_virt) ? dist_phy : dist_virt;
-    }else{
-        // only physical readings
-        dist_final = dist_phy;
-    }
+   if(dist_lock == 0){
+      // have both sensor readings
+      dist_final =  (dist_phy < dist_virt) ? dist_phy : dist_virt;
+   }else{
+      // only physical readings received
+      dist_final = dist_phy;
+   }
+   #else
+   // No virtual sensor support
+   dist_final = dist_phy;
+   #endif
 
-    Serial.printf("Distance: phy: %d virt: %d | final: %d\n\n", dist_phy, dist_virt, dist_final);
+   Serial.printf("Distance: phy: %d virt: %d | final: %d\n\n", dist_phy, dist_virt, dist_final);
 
-    display_clear();
-    display_print(0, dist_phy);
-    display_print(1, dist_virt);
+   display_clear();
+   display_print(0, dist_phy);
+   display_print(1, dist_virt);
 
-    return dist_final;
+   return dist_final;
 }
-
 
 #endif
--- firmware/mqtt/mqtt_proximity.cpp
+++ firmware/mqtt/mqtt_proximity.cpp
@@ -10,6 +10,7 @@ struct Proximity proximity_virt;
 #ifdef ENABLE_MQTT
 
 void mqtt_proximity_handle(char* msg){
+   #ifdef ENABLE_VIRT_READINGS
    Serial.printf("proximity update from server received: %s n", msg);
 
    // TODO: read directly into proximity_virt
@@ -33,9 +34,11 @@ void mqtt_proximity_handle(char* msg){
       proximity_virt.BackRight = 0;
       Serial.print(F("Error_ProximityResponseArgs"));
    }
+   #endif
 }
 
 void proximity_read(proximity_t* proximity){
+   #ifdef ENABLE_VIRT_READINGS
 
    // Publish: sensor/proximity
    //      { "id":0, "dist":0 }
@@ -43,15 +46,15 @@ void proximity_read(proximity_t* proximity){
    sprintf(tempString2, "{\"id\":\"%d\", \"reality\":\"M\"}", mqtt_robot_id);
 
    proximity_lock=1;
-   mqtt_publish(tempString1, tempString2, false);
 
+   mqtt_publish(tempString1, tempString2, false);
    mqtt_wait(&dist_lock);
 
-   if(proximity_lock == 0){
-      // have a reply
-   }else{
-      // no reply
-   }
+   // if(proximity_lock == 0){
+   //    // have a reply
+   // }else{
+   //    // no reply
+   // }
 
    Serial.printf("Proximity: %u %u %u %u %u\n\n", proximity_virt.BackLeft,proximity_virt.Left,proximity_virt.Front,proximity_virt.Right,proximity_virt.BackRight);
 
@@ -59,7 +62,17 @@ void proximity_read(proximity_t* proximity){
    // display_print(0, dist_phy);
    // display_print(1, dist_virt);
 
+   #else
+   // Sensor is not available, return value is 65535
+   proximity_virt.BackLeft = -1;
+   proximity_virt.Left = -1;
+   proximity_virt.Front = -1;
+   proximity_virt.Right = -1;
+   proximity_virt.BackRight = -1;
+
+   #endif
 }
+
 
 
 #endif
